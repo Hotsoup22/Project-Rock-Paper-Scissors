52b7a43fe05fdef7f9563c49bf3e077f7aeb14bc
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-05-15 13:16:43 -0700

    This is the initial upload to Github, for UW bootcamp

---
+++ miniproject.js
@@ -0,0 +1,24 @@
+
+var playerChoice = [ "0" , "1", "2"];
+var computerChoice = ["0", "1" , "2"]; 
+
+var computerChoice = Math.floor(Math.random() * computerChoice.length);
+
+cd
+if (playerChoice == computerChoice) {
+    console.log("You have tied against the computer");
+} else if (playerChoice == 1 && computerChoice == 0) {
+    console.log("You have chosen paper.");
+    console.log("The computer has chosen rock.");
+    console.log("You have won against the computer");
+} else if (playerChoice == 2 && computerChoice ==1) {
+    console.log("You have chosen scissors.");
+    console.log("The computer has chosen paper");
+    console.log("You have won against ther computer");
+} else if (playerChoice == 0 && computerChoice == 2) {
+    console.log("You have chosen rock.");
+    console.log("The computer has chosen scissors");
+    console.log("You have won against the computer");
+} else {
+    console.log("you have lost against the computer");
+}

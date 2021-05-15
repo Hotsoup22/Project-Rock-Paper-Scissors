ad9a7cdb7dabc88f8e34b44da2b916079c163e92
hotsoup42 <jose22gw@gmail.com>
2021-05-15 13:14:11 -0700

    begining comments

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//create the list of 'computer choices'
+//ask  the user for the 'userChoice'
+//Randomly generate the computers choice
+	
+	//randomly generate an index (with array?)
+	// use the indexc to select from 'computerchoices'
+
+//determine the result of the game, by comparing 'userChoice'against 'computerchoice' to determine a 'win', 'loss', or 'tie'
